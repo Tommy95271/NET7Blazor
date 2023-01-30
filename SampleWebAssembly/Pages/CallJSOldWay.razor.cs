bdb1c627fb7480c87c37225481f476a3c9f466be
謝志斌 Tommy <tommy@miniasp.com>
2023-01-30 18:39:50 +0800

    Benchmark JS old way

---
+++ SampleWebAssembly/Pages/CallJSOldWay.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+
+namespace SampleBlazorWebAssembly.Pages
+{
+    public partial class CallJSOldWay
+    {
+        private IJSObjectReference module;
+        protected override async Task OnInitializedAsync()
+        {
+            module = await JS.InvokeAsync<IJSObjectReference>("import", "../Pages/CallJSOldWay.razor.js");
+
+            for (int i = 0; i < 10; i++)
+            {
+                await module.InvokeVoidAsync("showMessage");
+            }
+        }
+    }
+}
---
+++ SampleWebAssembly/Pages/CallJSOldWay.razor.js
@@ -0,0 +1,5 @@
+﻿export function showMessage() {
+    console.time("JS old way");
+    document.getElementById("result2").innerText = "Hello from CallJSOldWay.razor.js!";
+    console.timeEnd("JS old way");
+}
