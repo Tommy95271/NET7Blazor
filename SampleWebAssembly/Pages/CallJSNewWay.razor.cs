c292365cbb3c4376de9b53155508f6a5a910ad00
謝志斌 Tommy <tommy@miniasp.com>
2023-01-30 18:36:13 +0800

    Benchmark JS new way

---
+++ SampleWebAssembly/Pages/CallJSNewWay.razor.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
+
+namespace SampleBlazorWebAssembly.Pages
+{
+    [SupportedOSPlatform("browser")]
+    public partial class CallJSNewWay
+    {
+        [JSImport("showMessage", "CallJSNewWay")]
+        internal static partial string ShowWelcomeMessage();
+    }
+}
---
+++ SampleWebAssembly/Pages/CallJSNewWay.razor.js
@@ -0,0 +1,5 @@
+﻿export function showMessage() {
+    console.time("JS new way");
+    document.getElementById("result1").innerText = "Hello from CallJSNewWay.razor.js!";
+    console.timeEnd("JS new way");
+}
