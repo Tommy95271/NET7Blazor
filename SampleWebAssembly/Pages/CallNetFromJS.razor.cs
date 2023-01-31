0b4619654757f35f27d8cfb7c70fbc3ffc0e038b
Tommy <Tommy@miniasp.com>
2023-01-31 17:37:17 +0800

    Call .NET from JS

---
+++ SampleWebAssembly/Pages/CallNetFromJS.razor.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
+
+namespace SampleBlazorWebAssembly.Pages
+{
+    [SupportedOSPlatform("browser")]
+    public partial class CallNetFromJS
+    {
+        [JSImport("setMessage", "CallNetFromJS")]
+        internal static partial void SetWelcomeMessage();
+
+        [JSExport]
+        internal static string GetMessageFromDotnet()
+        {
+            return "Olá do Blazor!";
+        }
+    }
+}
