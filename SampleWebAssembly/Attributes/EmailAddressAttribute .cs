6cb1e2a14e108e6fd7f0c7dca1faa3e771892b62
Tommy <Tommy@miniasp.com>
2023-03-03 19:46:06 +0800

    Custom validation attributes

---
+++ SampleWebAssembly/Attributes/EmailAddressAttribute .cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+/// <summary>
+/// Verify if email address is valid.
+/// </summary>
+public class CustomEmailAddressAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value != null)
+        {
+            var email = value?.ToString();
+
+            if (!Regex.IsMatch(email, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$"))
+            {
+                return new ValidationResult("不合法的 email 格式");
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
